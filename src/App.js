65e487c4efa132654e2b7d0022eb2e5ccf9ef816
ANUBHAV VERMA <114356733+Anubhav1195@users.noreply.github.com>
2024-06-14 11:15:49 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Anubhav verma is a good student, he lived in Lakhimpur Kheri And His Friend Yogendra verma ji,
+         Anubhav verma is a good student, he lived in Lakhimpur Kheri And His Friend Yogendra verma , Adersh , bhaiya,
         </p>
         <a
           className="App-link"
